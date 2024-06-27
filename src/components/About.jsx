71c7593dc13326ef75346c15d5398a7d401af8f0
Alan Sobchacki <a.sobchack@gmail.com>
2024-06-27 18:14:31 -0300

    Start redesigning the about page.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,13 +3,28 @@ import styles from "../styles/Shop.module.css";
 function About() {
   return (
     <div className={styles.main}>
-      <p>I am the about page!</p>
+      <h1>Hello!</h1>
+      <p>
+        You&apos;re here either because you clicked on the <i>About</i> button
+        in the nav bar...{" "}
+      </p>
+      <p>
+        Or because you actually bothered to test how the shopping cart works!
+      </p>
+      <p>Either way, I&apos;m glad to have you here.</p>
+      <p>
+        This is a fictional e-commerce store, created as part of a course I'm
+        taking. So, if you were hoping to buy something, sorry to burst your
+        bubble!
+      </p>
+      <p>
+        But hey, feel free to browse around, add items to your cart, and enjoy
+        the experience. Who knows, maybe you&apos;ll find some inspiration for
+        your own projects!
+      </p>
+      <p>Thanks for stopping by, and happy coding!</p>
     </div>
   );
 }
 
 export default About;
-
-// to-do:
-// add something funny/better as a content for your about us page
-// improve css rules by giving this file its own css module
