ac74010022853c2908f75243b5f16fd06c1c10e7
Alan Sobchacki <a.sobchack@gmail.com>
2024-06-26 21:30:21 -0300

    Finish decorating the footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,26 @@
 import styles from "../styles/Footer.module.css";
 import githubLogo from "../assets/github-mark-white.png";
+import linkedinLogo from "../assets/linkedin.png";
 
 function Footer() {
   return (
     <div className={styles.footer}>
       <p>
-        This is a fictional store built as an assignment from The Odin Project.
+        This is a fictional store built as an assignment from{" "}
+        <a href="https://www.theodinproject.com/dashboard">The Odin Project.</a>
       </p>
-      <p>https://github.com/alansobchacki</p>
-      <img src={githubLogo} className={styles.logo}></img>
+      <div id={styles.logos}>
+        <a href="https://github.com/alansobchacki" target="_blank">
+          <img src={githubLogo} id={styles.github} alt="GitHub Profile" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/alansobchacki/?locale=en_US"
+          target="_blank"
+        >
+          <img src={linkedinLogo} id={styles.linkedin} alt="LinkedIn Profile" />
+        </a>
+      </div>
+      <p>&copy; 2024 Alan Sobchacki. All rights reserved.</p>
     </div>
   );
 }
